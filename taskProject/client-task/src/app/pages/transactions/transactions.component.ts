2f70552f6f844506ba801e88cec0c5c766087258
Valeriya <229286108+vppetkova@users.noreply.github.com>
2026-04-06 04:47:07 +0300

    Add transactions logic

--- taskProject/client-task/src/app/pages/transactions/transactions.component.ts
+++ taskProject/client-task/src/app/pages/transactions/transactions.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Transaction } from '../../models/transaction';
+import { ClientService } from '../../client.service';
 
 @Component({
   selector: 'app-transactions',
@@ -7,4 +9,43 @@ import { Component } from '@angular/core';
   templateUrl: './transactions.component.html',
   styleUrl: './transactions.component.css'
 })
-export class TransactionsComponent {} 
+export class TransactionsComponent {
+// export class TransactionsComponent implements OnInit {
+  // transactions: Transaction[] | null = [];
+  // filteredText: string = '';
+  // openTransactionDetails: Transaction | null = null;
+
+  // constructor(private clientService: ClientService) {}
+
+  // ngOnInit(): void {
+  //   this.clientService.getTransactions().subscribe((data) => {
+  //     this.transactions = data;
+  //   });
+  // }
+
+  // get filteredTransactions() {
+  //   return this.transactions?.filter(t => 
+  //     t.name.toLowerCase().includes(this.filteredText.toLowerCase()) || 
+  //     t.status.toLowerCase().includes(this.filteredText.toLowerCase())) 
+  // }
+
+  // showModal(t: Transaction) {
+  //   this.openTransactionDetails = t;
+  // }
+
+  // closeModal() {
+  //   this.openTransactionDetails = null;
+  // }
+
+  // getStatusColor(status: string) {
+  //   if (status === 'Completed') {
+  //     return 'green';
+  //   } else if (status === 'Pending') {
+  //     return 'amber';
+  //   } else if (status === 'Failed') {
+  //     return 'red';
+  //   } else {
+  //     return 'black';
+  //   }
+  // }
+}  
