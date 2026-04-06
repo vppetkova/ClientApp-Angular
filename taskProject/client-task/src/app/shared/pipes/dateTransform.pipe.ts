1dbd03caa9c4dcd87c7c7bd33f138d652cf4dfb2
Valeriya <229286108+vppetkova@users.noreply.github.com>
2026-04-06 05:15:37 +0300

    Create dateTransform pipe

---
+++ taskProject/client-task/src/app/shared/pipes/dateTransform.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+    name: 'dateTransform',
+    standalone: true,
+})
+
+export class DateTransformPipe implements PipeTransform {
+    transform(value: string): string {
+        if (!value) {
+            return '';
+        }
+
+        return value.split('-'). reverse(). join(' . ');
+    }
+} 
