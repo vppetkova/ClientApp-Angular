3b75d8ec3901f73328952afa6f77dc070244554f
Valeriya <229286108+vppetkova@users.noreply.github.com>
2026-04-06 03:56:58 +0300

    Create ClientService

---
+++ taskProject/client-task/src/app/client.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Client } from "./models/client";
+// import { catchError } from 'rxjs';
+import { Transaction } from "./models/transaction";
+
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class ClientService {
+    constructor(private http: HttpClient) {}
+
+    getClient() {
+        return this.http.get<Client>('assets/client.json'); //handle errors
+    }
+
+    getTransactions() {
+        return this.http.get<Transaction[]>('assets/transactions.json'); //handle errors
+    }
+}
