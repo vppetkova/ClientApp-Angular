3b4a2e7ddde74f731fa509ee27cac10161178e67
Valeriya <229286108+vppetkova@users.noreply.github.com>
2026-04-06 03:14:16 +0300

    Configure navbar routing

--- taskProject/client-task/src/app/app.routes.ts
+++ taskProject/client-task/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ClientComponent } from './pages/client/client.component';
+import { TransactionsComponent } from './pages/transactions/transactions.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/client', pathMatch: 'full' },
+    { path: 'client', component: ClientComponent },
+    { path: 'transactions', component: TransactionsComponent },
+    { path: '**', redirectTo: '/client' },
+];
